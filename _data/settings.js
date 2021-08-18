4004a6590c2bf28e76a44103e857e89b344f5cdf
Dan Urbanowicz <dan.urbanowicz@gmail.com>
2021-08-18 10:45:48 +0200

    Add meetup.com page url

--- _data/settings.js
+++ _data/settings.js
@@ -4,6 +4,7 @@ module.exports = {
   url: "https://jamstackbarcelona.dev",
   domain: "jamstackbarcelona.dev",
   email: "jamstackbarcelona@gmail.com",
+  meetup: "https://www.meetup.com/jamstack-barcelona/",
   buildTime: new Date(),
   currentYear: (new Date()).getFullYear()
 }
